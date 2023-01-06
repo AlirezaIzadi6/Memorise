3fafd2e4f164071be22343ecad678f006b33e0d8
Alireza Izadi <alirezaizadi199912@gmail.com>
2023-01-07 02:36:28 +0330

    created basic models for deck, flashcard and holder

--- memrise/web/models.py
+++ memrise/web/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Deck(models.Model):
+    title = models.CharField(max_length=63)
+    description = models.CharField(max_length=1023)
+    users = models.ManyToManyField(User)
+    
+    def __str__(self):
+        return self.title
+
+class Flashcard(models.Model):
+    question = models.CharField(max_length=63)
+    answer = models.CharField(max_length=63)
+    description = models.CharField(max_length=255)
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+class Holder(models.Model):
+    learned = models.IntegerField(default=0)
+    NumOfReviews = models.IntegerField(default=0)
+    LastReview = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    flashcard = models.ForeignKey(Flashcard, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.user
+
