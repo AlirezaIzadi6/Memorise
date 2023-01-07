7ec9d215962156008774223ad9ec4c5c264b1e1d
Alireza Izadi <alirezaizadi199912@gmail.com>
2023-01-07 23:34:05 +0330

    added show decks page

--- memrise/web/views.py
+++ memrise/web/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 
+from .models import *
+
 tAddress = {
     'tIndex': 'web\\index.html',
+    'tShowdecks': 'web\\showdecks.html',
+    'tError_login': 'web\\error_login.html',
 }
 
 def index(request):
     user = request.user
     context = {'user': user}
     return render(request, tAddress['tIndex'], context)
+
+def showdecks(request):
+    if request.user.is_authenticated:
+        decks = Deck.objects.filter(users=request.user)
+        context = {'decks': decks}
+        return render(request, tAddress['tShowdecks'], context)
+    else:
+        return render(request, tAddress['tError_login'], {})
