d86c398d17f6715b645fd7a6b1b43f69ef164538
Alireza Izadi <alirezaizadi199912@gmail.com>
2023-01-08 03:21:13 +0330

    some modifications to make message section work

--- memrise/web/views.py
+++ memrise/web/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 
 from .models import *
 
+messages = []
 tAddress = {
     'tIndex': 'web\\index.html',
     'tShowdecks': 'web\\show-decks.html',
@@ -13,36 +14,53 @@ tAddress = {
 }
 
 def index(request):
+    global messages
     user = request.user
-    context = {'user': user}
+    context = {'user': user, 'messages': messages}
+    messages = []
     return render(request, tAddress['tIndex'], context)
 
 def showdecks(request):
+    global messages
     if request.user.is_authenticated:
         decks = Deck.objects.filter(users=request.user)
-        context = {'decks': decks}
+        context = {'decks': decks, 'messages': messages}
+        messages = []
         return render(request, tAddress['tShowdecks'], context)
     else:
         return render(request, tAddress['tError_login'], {})
 
 def showdeck(request, d_id):
+    global messages
     deck = Deck.objects.get(id=d_id)
     cards = Flashcard.objects.filter(deck=deck)
-    context = {'deck': deck, 'cards': cards}
+    context = {'deck': deck, 'cards': cards, 'messages': messages}
+    messages = []
     return render(request, tAddress['tShowdeck'], context)
 
 def addbatchflashcard(request, d_id):
+    global messages
     deck = Deck.objects.get(id=d_id)
-    context = {'deck': deck}
+    context = {'deck': deck, 'messages': messages}
+    messages = []
     return render(request, tAddress['tAddbatchflashcard'], context)
 
 def submitbatchflashcard(request, d_id):
+    global messages
     deck = Deck.objects.get(id=d_id)
-    data = request.POST['data']
-    for d in data.split('\n'):
+    data = request.POST['data'].split('\n')
+    counter = 0
+    for d in data:
         parsed = d.split(',')
         if len(parsed) < 3:
             continue
         card = Flashcard(deck=deck, question=parsed[0], answer=parsed[1], description=parsed[2])
         card.save()
+        counter += 1
+    if request.POST['data'] == '':
+        messages.append('ورودی از سمت شما ارسال نشده و هیچ کارتی به مجموعه اضافه نشد.')
+    elif counter == 0:
+        messages.append('هیچ کارتی به مجموعه اضافه نشد. بعد از چک کردن ورودی دوباره تلاش کنید.')
+    else:
+        messages.append('از ' + str(len(data)) + ' ورودی ارسال شده ' + str(counter) + ' کارت با موفقیت اضافه شد.')
     return HttpResponseRedirect(reverse('showdeck', args=(deck.id,)))
