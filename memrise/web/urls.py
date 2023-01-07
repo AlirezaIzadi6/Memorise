4caf24a9a51884e00bdfad590a3ee795a8b69af9
Alireza Izadi <alirezaizadi199912@gmail.com>
2023-01-08 00:28:40 +0330

    added a page to add batch flashcards

--- memrise/web/urls.py
+++ memrise/web/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('decks', views.showdecks, name='showdecks'),
+    path('deck/<int:d_id>/addbatchflashcard', views.addbatchflashcard, name='addbatchflashcard'),
+    path('deck/<int:d_id>/submitbatchflashcard', views.submitbatchflashcard, name='submitbatchflashcard'),
     path('deck/<int:d_id>', views.showdeck, name='showdeck'),
 ]
--- memrise/web/views.py
+++ memrise/web/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 from .models import *
 
@@ -7,6 +9,7 @@ tAddress = {
     'tShowdecks': 'web\\showdecks.html',
     'tError_login': 'web\\error_login.html',
     'tShowdeck': 'web\\showdeck.html',
+    'tAddbatchflashcard': 'web\\addbatchflashcard.html',
 }
 
 def index(request):
@@ -27,3 +30,19 @@ def showdeck(request, d_id):
     cards = Flashcard.objects.filter(deck=deck)
     context = {'deck': deck, 'cards': cards}
     return render(request, tAddress['tShowdeck'], context)
+
+def addbatchflashcard(request, d_id):
+    deck = Deck.objects.get(id=d_id)
+    context = {'deck': deck}
+    return render(request, tAddress['tAddbatchflashcard'], context)
+
+def submitbatchflashcard(request, d_id):
+    deck = Deck.objects.get(id=d_id)
+    data = request.POST['data']
+    for d in data.split('\n'):
+        parsed = d.split(',')
+        if len(parsed) < 3:
+            continue
+        card = Flashcard(deck=deck, question=parsed[0], answer=parsed[1], description=parsed[2])
+        card.save()
+    return HttpResponseRedirect(reverse('showdeck', args=(deck.id,)))
