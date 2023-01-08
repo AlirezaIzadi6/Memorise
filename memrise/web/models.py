2b66ffce33a0d38196cb82762db7c7aebccc2c8b
Alireza Izadi <alirezaizadi199912@gmail.com>
2023-01-08 04:25:54 +0330

    modified models fields about null and blank parameters

--- memrise/web/models.py
+++ memrise/web/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 class Deck(models.Model):
     title = models.CharField(max_length=63)
-    description = models.CharField(max_length=1023)
-    users = models.ManyToManyField(User)
+    description = models.CharField(max_length=1023, null=True, blank=True)
+    users = models.ManyToManyField(User, null=True, blank=True)
     
     def __str__(self):
         return self.title
@@ -18,7 +18,7 @@ class Deck(models.Model):
 class Flashcard(models.Model):
     question = models.CharField(max_length=63)
     answer = models.CharField(max_length=63)
-    description = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, null=True, blank=True)
     deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
     
     def __str__(self):
@@ -27,7 +27,7 @@ class Flashcard(models.Model):
 class Holder(models.Model):
     learned = models.IntegerField(default=0)
     NumOfReviews = models.IntegerField(default=0)
-    LastReview = models.DateTimeField()
+    LastReview = models.DateTimeField(null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     flashcard = models.ForeignKey(Flashcard, on_delete=models.CASCADE)
     
